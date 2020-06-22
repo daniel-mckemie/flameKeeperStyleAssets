9cc5e29df619a0bf00118c29e551bd22c9369594
Daniel McKemie <daniel.mckemie@gmail.com>
2020-06-21 22:35:19 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,55 @@
+let wavesurfer0 = WaveSurfer.create({
+  container: '.waveform'
+});
+wavesurfer0.load(`https://fk-audio.s3.amazonaws.com/1592190864257-0-noise2.mp3`);
+wavesurfer0.on('ready', function () {
+  wavesurfer0.play();
+});
+
+let wavesurfer1 = WaveSurfer.create({
+  container: '.waveform'
+});
+wavesurfer1.load(`https://fk-audio.s3.amazonaws.com/1592189157314-5-noise4.mp3`);
+wavesurfer1.on('ready', function () {
+  wavesurfer1.play();
+});
+
+let wavesurfer2 = WaveSurfer.create({
+  container: '.waveform'
+});
+wavesurfer2.load(`https://fk-audio.s3.amazonaws.com/1592190318551-1-noise1.mp3`);
+wavesurfer2.on('ready', function () {
+  wavesurfer2.play();
+});
+
+let wavesurfer3 = WaveSurfer.create({
+  container: '.waveform'
+});
+wavesurfer3.load(`https://fk-audio.s3.amazonaws.com/1592190864257-0-noise2.mp3`);
+wavesurfer3.on('ready', function () {
+  wavesurfer3.play();
+});
+
+let wavesurfer4 = WaveSurfer.create({
+  container: '.waveform'
+});
+wavesurfer4.load(`https://fk-audio.s3.amazonaws.com/1592190318551-1-noise1.mp3`);
+wavesurfer4.on('ready', function () {
+  wavesurfer4.play();
+});
+
+let wavesurfer5 = WaveSurfer.create({
+  container: '.waveform'
+});
+wavesurfer5.load(`https://fk-audio.s3.amazonaws.com/1592190710699-1-noise8.mp3`);
+wavesurfer5.on('ready', function () {
+  wavesurfer5.play();
+});
+
+let wavesurfer6 = WaveSurfer.create({
+  container: '.waveform'
+});
+wavesurfer6.load(`https://fk-audio.s3.amazonaws.com/1592189157314-5-noise4.mp3`);
+wavesurfer6.on('ready', function () {
+  wavesurfer6.play();
+});
